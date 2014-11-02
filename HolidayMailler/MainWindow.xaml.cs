3856610d65e94c55b9fe3068eca43a0f4db5faac
marcus1255 <marcus1255@gmail.com>
2014-11-01 22:19:50 -0700

    Test GUI commit
    Added a DataGrid to GUI, just to test the commiting process

--- HolidayMailler/MainWindow.xaml.cs
+++ HolidayMailler/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace HolidayMailler
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
