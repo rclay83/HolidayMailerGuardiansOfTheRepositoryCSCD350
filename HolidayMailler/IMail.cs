c12f1fdc4736ed24f6639a76135703f7f001a41f
marcus1255 <marcus1255@gmail.com>
2014-11-02 12:17:24 -0800

    IMail interface

---
+++ HolidayMailler/IMail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMailler
+{
+    interface IMail
+    {
+        public string[] GetRecipients ();
+
+        public string GetSubject ();
+
+        public string GetBody ();
+
+        public string Sender
+        {
+            get;
+            set;
+        }
+    }
+}
