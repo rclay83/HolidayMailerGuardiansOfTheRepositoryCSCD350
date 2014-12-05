812902c8ff53efde0fee85c2153ed33bb57f5d22
marcus1255 <marcus1255@gmail.com>
2014-12-04 16:44:40 -0800

    Icon
    icon

--- HolidayMailler/Mock.cs
+++ HolidayMailler/Mock.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Email;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Mail;
 using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
 
 /*  Holiday Mailer
  *  Guardians of the Repository
